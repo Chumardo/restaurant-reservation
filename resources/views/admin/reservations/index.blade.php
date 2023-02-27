170c8b447fa2702f058561972137803f9a119718
chumardo <dachigiorgadze98@gmail.com>
2023-02-27 20:28:10 +0400

    display reservations

--- resources/views/admin/reservations/index.blade.php
+++ resources/views/admin/reservations/index.blade.php
@@ -16,62 +16,48 @@
           <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" class="px-6 py-3">
-                Product name
+                Full Name
               </th>
               <th scope="col" class="px-6 py-3">
-                Color
+                Phone Number
               </th>
               <th scope="col" class="px-6 py-3">
-                Category
+                Email
               </th>
               <th scope="col" class="px-6 py-3">
-                Price
+                Reservation Date
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Table ID
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Guests
               </th>
             </tr>
           </thead>
           <tbody>
+            @foreach ($reservations as $reservation)
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Apple MacBook Pro 17"
-              </th>
-              <td class="px-6 py-4">
-                Silver
+              <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {{ $reservation->first_name }} {{ $reservation->last_name }}
               </td>
               <td class="px-6 py-4">
-                Laptop
+                {{ $reservation->tel_number }}
               </td>
               <td class="px-6 py-4">
-                $2999
-              </td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Microsoft Surface Pro
-              </th>
-              <td class="px-6 py-4">
-                White
+                {{ $reservation->email }}
               </td>
               <td class="px-6 py-4">
-                Laptop PC
+                {{ $reservation->res_date }}
               </td>
               <td class="px-6 py-4">
-                $1999
+                {{ $reservation->table_id }}
+              </td>
+              <td class="px-6 py-4">
+                {{ $reservation->guest_number }}
               </td>
             </tr>
-            <tr class="bg-white dark:bg-gray-800">
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Magic Mouse 2
-              </th>
-              <td class="px-6 py-4">
-                Black
-              </td>
-              <td class="px-6 py-4">
-                Accessories
-              </td>
-              <td class="px-6 py-4">
-                $99
-              </td>
-            </tr>
+            @endforeach
           </tbody>
         </table>
       </div>
