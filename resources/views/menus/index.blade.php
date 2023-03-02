175b228b1403fa0ad6eae07deb9d21152c8d6a86
chumardo <dachigiorgadze98@gmail.com>
2023-03-02 16:50:47 +0400

    display menus for guests

---
+++ resources/views/menus/index.blade.php
@@ -0,0 +1,22 @@
+<x-guest-layout>
+  <div class="container w-full px-5 py-6 mx-auto">
+    <div class="grid lg:grid-cols-4 gap-y-6">
+      @foreach ($menus as $menu)
+      <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg divide-y-2 divide-black">
+        <img class="w-full h-48 rounded-t-lg" src="{{ Storage::url($menu->image) }}" alt="Image" />
+        <div class="px-6 py-4 bg-purple-900 text-center">
+          <h4 class="mb-3 text-xl font-semibold tracking-tight text-white uppercase">
+            {{ $menu->name }}</h4>
+          <p class="leading-normal text-gray-400">
+            {{ $menu->description }}
+          </p>
+        </div>
+        <div class="p-4 bg-green-900 text-center rounded-b-lg">
+          <span class="text-xl text-white text-center">${{ $menu->price }}</span>
+        </div>
+      </div>
+      @endforeach
+
+    </div>
+  </div>
+</x-guest-layout>
