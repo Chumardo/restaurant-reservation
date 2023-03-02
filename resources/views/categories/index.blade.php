f4775e6e9b24c20acfe79e17c2cdca998e0ccc2d
chumardo <dachigiorgadze98@gmail.com>
2023-03-02 16:06:23 +0400

    style menu card and fix category card error

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -2,9 +2,9 @@
   <div class="container w-full px-5 py-6 mx-auto">
     <div class="grid lg:grid-cols-4 gap-y-6">
       @foreach ($categories as $category)
-      <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg bg-purple-900">
+      <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
         <img class="w-full h-48 rounded-t-lg" src="{{ Storage::url($category->image) }}" alt="Image" />
-        <div class="text-center">
+        <div class="text-center bg-purple-900">
 
           <a href="{{ route('categories.show', $category->id) }}">
             <h4 class="p-3 text-xl font-semibold tracking-tight text-green-500 hover:text-white uppercase">
