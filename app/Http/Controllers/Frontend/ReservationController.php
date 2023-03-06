baef89e9d3d9a115df1db1a6a9c5103516fc826f
chumardo <dachigiorgadze98@gmail.com>
2023-03-06 17:08:27 +0400

    add reservation date restriction

--- app/Http/Controllers/Frontend/ReservationController.php
+++ app/Http/Controllers/Frontend/ReservationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Reservation;
 use App\Models\Table;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
@@ -12,6 +13,8 @@ class ReservationController extends Controller
     public function index()
     {
         $tables = Table::all();
-        return view('reservations.index', compact('tables', 'reservation'));
+        $min_date = Carbon::today();
+        $max_date = Carbon::now()->addWeek();
+        return view('reservations.index', compact('tables', 'min_date', 'max_date'));
     }
 }
--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -63,6 +63,7 @@
                   </label>
                   <div class="mt-1">
                     <input type="datetime-local" id="res_date" name="res_date"
+                      min="{{ $min_date->format('Y-m-d\TH:i:s') }}" max="{{ $max_date->format('Y-m-d\TH:i:s') }}"
                       class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                   </div>
                   @error('res_date')
